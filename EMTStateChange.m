04bc2e492104a8bed87b81e92ea206cf937c1650
Kristin Kim <99146622+KimK22@users.noreply.github.com>
2024-12-02 10:54:28 -0500

    Update EMTStateChange.m

--- EMTStateChange.m
+++ EMTStateChange.m
@@ -1,8 +1,9 @@
 function [CellState, Cstate, Ctgfb, Dtgfb] = EMTStateChange(CellState, Param, Cstate, Ctgfb, Dtgfb, t, Tfinal, dt)
-% EMTStateChange: Function that runs EMT characterization based on changes in concentration of N-Cadherin.
+% EMTStateChange: Function that runs EMT characterization based on changes in concentration of N-Cadherin. ODE is solved
+%using Euler Method.
 
 numCells = length(CellState.state);
-CellState.Pop(t, :) = CellState.Pop(t - 1, :);
+CellState.Pop(t, :) = [0, 0, 0, 0]; % CellState.Pop(t - 1, :);
     
 for v = 1:numCells % for the number of cells in the system
         
@@ -14,49 +15,33 @@ for v = 1:numCells % for the number of cells in the system
         [Ctgfb(i,j,k), CellState.conc(v,:)] = TGFB_ODE(t, dt:dt:Tfinal, dt, CellState.conc(v,:), Ctgfb(i,j,k));
         
         CellState.Ncad(v, 2) = CellState.conc(v,8);
-        CellState.Ecad(v) = CellState.conc(v,7);
-        CellState.Snail(v) = CellState.conc(v,2);
         
         CellState.p(v)       = CellState.Ncad(v,2) / Param.NcadMax;
-        if CellState.p(v) > 1
-            CellState.p(v) = 1;
-        end
-        
         Dtgfb(i,j,k)         = Param.Dcell*(1-CellState.p(v));
+        CellState.Dcell(v)   = Dtgfb(i,j,k);
         CellState.Ctgfb(v)   = Ctgfb(i,j,k);
 
-      % Changing Cell State
-            % E: < 1.5;    P: 1.5 < X < 2.4;  M: 2.4 < X < 3.1515
-            Cstate_prev = CellState.state(v);
-                        
-            if CellState.Ncad(v,2) >= 2.4  || CellState.state(v) >= 3 % Mesenchymal State
-                CellState.state(v) = 3;
-                Cstate(i,j,k) = 3;
+        % Changing Cell State
+        % E: < 1.5;    P: 1.5 < X < 2.4;  M: 2.4 < X < 3.1515
+        if CellState.Ncad(v,2) >= 2.4 || CellState.state(v) == 3 % Mesenchymal State
+            CellState.state(v) = 3;
+            Cstate(i,j,k) = 3;
+    
+        elseif CellState.Ncad(v,2) >= 1.5 && CellState.Ncad(v,2) < 2.4 % Partial State
+            CellState.state(v) = 2;
+            Cstate(i,j,k) = 2;
+    
+        elseif CellState.Ncad(v,2) < 1.5 % Epithelial State
+            CellState.state(v) = 1;
+            Cstate(i,j,k) = 1;
+    
+        end
 
-                if CellState.Ncad(v,1) < 2.4 && CellState.Ncad(v,2) >= 2.4 && Cstate_prev == 2 % Partial to Mesenchymal
-                    CellState.Pop(t, 2) =  CellState.Pop(t ,2) - 1;
-                    CellState.Pop(t, 3) =  CellState.Pop(t, 3) + 1;
-                end
-
-            elseif CellState.Ncad(v,2) >= 1.5 && CellState.Ncad(v,2) < 2.4 % Partial State
-                CellState.state(v) = 2;
-                Cstate(i,j,k) = 2;
-
-                if CellState.Ncad(v,1) < 1.5 && Cstate_prev == 1 % E to P
-                    CellState.Pop(t, 1) = CellState.Pop(t, 1) - 1;
-                    CellState.Pop(t, 2) = CellState.Pop(t, 2) + 1;
-                end
-
-            elseif CellState.Ncad(v,2) < 1.5 && CellState.state(v) < 3 % Epithelial State
-                CellState.state(v) = 1;
-                Cstate(i,j,k) = 1;
-                
-                if CellState.Ncad(v,1) >= 1.5 && CellState.Ncad(v,1) < 2.4 && Cstate_prev == 2 % P to E
-                    CellState.Pop(t, 1) = CellState.Pop(t, 1) + 1;
-                    CellState.Pop(t, 2) = CellState.Pop(t, 2) - 1; 
-                end         
-            end
-     end           
+        tempInd = CellState.state(v);
+        CellState.Pop(t, tempInd) = CellState.Pop(t, tempInd) + 1;
+        
+     end   
+     
 end
 
 CellState.Ncad(:,1) = CellState.Ncad(:,2); % Update Ncad concentrations
